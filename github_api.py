6f05c7fca0b9f09efc1b9b0edd5a318e5383dee0
Alex Calia <alex.m.calia@outlook.com>
2020-12-21 09:58:24 -0500

    filtering results

--- github_api.py
+++ github_api.py
@@ -5,10 +5,17 @@ def repos_with_most_stars():
     gh = "https://api.github.com/search/repositories"
     params = {"q": "stars:>50000"}
     response = requests.get(gh, params=params)
-    response_json = response.json()
-    print(response_json.keys())
+    response_json = response.json()["items"]
+    return response_json
 
 
 if __name__ == "__main__":
     # have a main method here
-    repos_with_most_stars()
+    results = repos_with_most_stars()
+
+    for result in results:
+        language = result['language']
+        stars = result["stargazers_count"]
+        name = result["name"]
+
+        print(f"=> {name} is a {language} repo with {stars} stars")
